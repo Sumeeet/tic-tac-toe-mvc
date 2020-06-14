a147d229cc8290eafae87e9ad7ff28b18a93ee33
Khanna <Sumeet.Khanna@wsp.com>
2020-06-14 20:12:10 +0530

    seperate board from game logic

--- board.js
+++ board.js
@@ -1,54 +1,55 @@
-/* eslint-disable no-unused-vars */
-exports.board = () => {
-  let board = []
-  let defaultCellValue
-  let boardDimension = 0
+exports.board = (dimension, rules) => {
+  let board = [];
 
-  function isBoardEmty () { return boardDimension === 0 }
+  // Initialize board
+  (() => {
+    const dim = parseInt(dimension)
+    if (isNaN(dim)) {
+      console.error(`${dimension} is not a valid number`)
+      return
+    }
 
-  function getRow (index) { return board.slice(index * boardDimension, index * boardDimension + boardDimension) }
+    board = Array(dimension * dimension).fill(' ')
+  })()
+
+  function isBoardEmty () { return dimension === 0 }
+
+  function getRow (index) { return board.slice(index * dimension, index * dimension + dimension) }
+
+  function IsValidIndex (index) { return (index >= 0 && index < dimension * dimension) }
 
   return {
-    /**
-     * Initialize the board with desired dimension i.e. if dimension is 3 then board will be 3X3
-     * and shall have 9 cells.
-     * @param {number} dimension dimension specifies the board size.
-     * @param {number} defaultState defaultState is a 1-dim array of default board state
-     */
-    initialize: function (dimension, defaultValue) {
-      const dim = parseInt(dimension)
-      if (isNaN(dim)) {
-        console.error(`${dimension} : is not a valid number`)
-        return
-      }
-
-      defaultCellValue = defaultValue
-      boardDimension = dimension
-      board = Array(dimension * dimension).fill(defaultValue)
-    },
 
     /**
      * Place appropriate values at board cell
      * @param {number} index index is a position in 1-dim array
      * @param {number} value value to be stored at array index
      */
-    makeMove: function (index, value) {
+    makeMove: (index, value) => {
       if (isBoardEmty()) {
-        console.error(`${board} : Empty borad. Initialize board first.`)
+        console.error(`Empty borad ${board}. Initialize board first.`)
+        return
       }
+
+      if (!IsValidIndex(index)) {
+        console.error(`Invalid cell index ${index}. Valid index is from 0 to ${dimension * dimension - 1}`)
+        return
+      }
+
+      rules.move(index, value, board)
     },
 
     /**
      * Reset board to default state
      */
-    reset: () => { board = Array(boardDimension * boardDimension).fill(defaultCellValue) },
+    reset: () => { board = Array(dimension * dimension).fill(' ') },
 
     /**
      * Print the board on console
      */
-    print: function () {
+    print: () => {
       if (isBoardEmty()) {
-        console.error(`${board} : Empty borad. Initialize board first.`)
+        console.error(`Empty borad ${board}. Initialize board first.`)
         return
       }
 
@@ -57,9 +58,9 @@ exports.board = () => {
         return cellLayout + getEmptyRow(nCells - 1, cellLayout)
       }
 
-      for (let index = 0; index < boardDimension; index++) {
+      for (let index = 0; index < dimension; index++) {
         console.log(` ${getRow(index).reduce((accum, value) => `${accum} | ${value}`)} |`)
-        console.log(getEmptyRow(boardDimension - 1, '---|'))
+        console.log(getEmptyRow(dimension - 1, '---|'))
       }
 
       console.log('')
--- test/tictactoe-test.js
+++ test/tictactoe-test.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-undef */
 const Board = require('../board')
+const Rules = require('../tictactoe-rules')
 
 describe('TicTacToe', () => {
   context('(PrintBoard)', () => {
     it('Initialize Board', () => {
-      const board = Board.board()
-      board.initialize(3, 8)
+      const board = Board.board(3, Rules.rules())
+      board.makeMove(8, 'X')
       board.print()
     })
   })
